b988fecc56b27ed36a64d0e45dbe77db37231fd7
Samuel García <samueljose.garcia@iesdonana.org>
2023-09-28 17:46:40 +0200

    Guardando estado...

--- aux_calculadorapoo.php
+++ aux_calculadorapoo.php
@@ -36,7 +36,8 @@ class Calculadora implements CalculadoraInterface
     }
 }
 
-class Validador {
+class Validador
+{
     public function valida($op1, $op2, $operacion, &$errores)
     {
         if (!is_numeric($op1) && $op1 === '') {
@@ -63,8 +64,58 @@ class Validador {
             $errores[] = 'No se puede dividir entre cero.';
         }
 
-        if (!in_array($operacion, Calculadora::OPS)) {
+        if (!in_array($operacion, Calculadora::OPS) || $operacion === '') {
             $errores[] = 'La operación no está permitida.';
         }
+    }
+}
+
+class VistaCalculadora
+{
+    public function mostrarFormulario($op1, $op2, $operador)
+    { ?>
+        <form action="" method="get">
+            <label for="op1">Operador 1</label>
+            <input type="number" name="op1" id="op1" value="<?= htmlspecialchars($op1 ?? ''); ?>">
+            <label for="op2">Operador 2</label>
+            <input type="number" name="op2" id="op2" value="<?= htmlspecialchars($op2 ?? ''); ?>">
+            <label for="op">Operación</label>
+            <select name="op" id="">
+                <?php foreach (Calculadora::OPS as $op) : ?>
+                    <option value="<?= htmlspecialchars($op) ?>" <?= Utilidad::seleccionaOpcion($operador, $op) ?>><?= $op ?></option>
+                <?php endforeach; ?>
+            </select>
+            <button type="submit">Calcular</button>
+        </form>
+    <?php
+    }
+
+    public function mostrarResultado(float $res): void
+    {
+        ?>
+        <p>El <strong>resultado</strong> es <strong><?= $res ?></strong></p>
+        <?php
+    }
+
+    public function mostrarErrores(array $errores): void
+    {
+        foreach($errores as $error){ ?>
+        <ul>
+            <li><?= $error; ?></li>
+        </ul>
+        <?php
+    }   }
+
+}
+
+class Utilidad {
+    public static function obtenerGet($argumento)
+    {
+        return isset($_GET[$argumento]) ? trim($_GET[$argumento]) : null;
+    }
+
+    public static function seleccionaOpcion($operador, $option)
+    {
+        return $operador == $option ? 'selected' : '';
     }
 }
--- calculadora_poo.php
+++ calculadora_poo.php
@@ -1,31 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylecalc.css">
     <title>Calculadora - POO</title>
 </head>
+
 <body>
     <?php
-        require 'aux_calculadorapoo.php';
-        $errores = [];
+    require 'aux_calculadorapoo.php';
+    $calculadora = new Calculadora();
+    $validador = new Validador();
+    $vista = new VistaCalculadora();
+    $utilidad = new Utilidad();
+    $errores = [];
+
+    $op1 = Utilidad::obtenerGet('op1');
+    $op2 = Utilidad::obtenerGet('op2');
+    $operador = Utilidad::obtenerGet('op');
+
+    $vista->mostrarFormulario($op1, $op2, $operador);
+
+
+
+    if (isset($op1, $op2, $operacion)) {
+        $validador->valida($op1, $op2, $operacion, $errores);
+        if (empty($errores)) {
+            try {
+                $resultado = $calculadora->calcular($op1, $op2, $op);
+                $vista->mostrarResultado($resultado);
+            } catch (\Throwable $th) {
+                $vista->mostrarErrores($errores);
+            }
+        }
+    }
+
     ?>
-    <h1>Calculadora</h1>
-    <cite>Siguiendo el paradigma de POO.</cite>
-    <form action="" method="get">
-        <label for="op1">Operador 1</label>
-        <input type="number" name="op1" id="op1">
-        <label for="op2">Operador 2</label>
-        <input type="number" name="op2" id="op2">
-        <label for="op">Operación</label>
-        <select name="op" id="">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-        </select>
-        <button type="submit">Calcular</button>
-    </form>
 </body>
+
 </html>
