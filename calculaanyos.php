33de53f6cf3f1e8f77dfe5d066c121cedd16fd2f
Samu G <samug@MacBook-Air-de-Samu.local>
2023-10-02 22:36:23 +0200

    Calculadora años. Recoge los datos y los mantiene seleccionados

--- calculaanyos.php
+++ calculaanyos.php
@@ -6,6 +6,93 @@
     <title>Calculadora de años</title>
 </head>
 <body>
+    <?php
+    /*
+        Apuntes:
+        - Se puede calcular la diferencia entre dos fecha usando el método diff de datetime.
+        - Se puede modificar objetos de fecha y hora usando el método modify. (para sumar años por ejemplo).
+
+    */
+
+        function obtener($arg)
+        {
+            return isset($_GET[$arg]) ? $_GET[$arg] : null;
+        }
+
+        function selected($valor, $option)
+        {
+            if ($valor == $option) {
+                return $valor == $option ? 'selected' : '';
+            }
+        }
+
+        $datetime = new DateTime();
+        $anyo_actual = $datetime->format('Y');
+        const MESES = [1 => 'enero',
+                            'febrero',
+                            'marzo',
+                            'abril',
+                            'mayo',
+                            'junio',
+                            'julio',
+                            'agosto',
+                            'septiembre',
+                            'octubre',
+                            'noviembre',
+                            'diciembre',
+                        ];
+
+
+
+
+    $dia =  obtener('dia');
+    $mes = obtener('mes');
+    $anyo = obtener('anyo');
+
+
+    ?>
+
+
+
     <center><h1>Calculadora de años.</h1></center>
+    <form action="" method="get">
+        <label for="dia">Selecciona tu día de nacimiento:</label>
+        <select name="dia" id="dia">
+            <?php
+                for ($i=1; $i <31 ; $i++) {
+                    ?>
+                    <option value="<?= $i ?>" <?= selected($dia, $i) ?>><?= $i ?></option>
+                    <?php
+                }
+            ?>
+        </select>
+        <label for="mes">Selecciona tu mes de nacimiento:</label>
+        <select name="mes" id="mes">
+            <?php
+                foreach (MESES as $mesfor) {
+                    ?>
+                    <option value="<?= $mesfor ?>" <?= selected($mes, $mesfor) ?>><?= $mesfor ?></option>
+                    <?php
+                }
+            ?>
+        </select>
+        <label for="anyo">Selecciona tu año:</label>
+        <select name="anyo" id="anyo">
+            <?php
+                for ($i=$anyo_actual; $i > ($anyo_actual - 50 )  ; $i--) {
+                    ?>
+                    <option value="<?= $i ?>" <?= selected($anyo, $i) ?>><?= $i ?></option>
+                    <?php
+                }
+            ?>
+        </select>
+        <br>
+        <br>
+        <button type="submit">Calcular</button>
+    </form>
+
+    <p><?= isset($dia) ? $dia: ''; ?></p>
+    <p><?= isset($mes) ? $mes: ''; ?></p>
+    <p><?= isset($anyo) ? $anyo: ''; ?></p>
 </body>
 </html>
