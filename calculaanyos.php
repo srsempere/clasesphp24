b4d59a8bb7ea9e7f5ac4ad84622edf2f02d4c440
Samuel Garcia <samueljose.garcia@iesdonana.org>
2023-10-02 21:18:15 +0200

    Calculadora de años. En proceso...

---
+++ calculaanyos.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora de años</title>
+</head>
+<body>
+    <center><h1>Calculadora de años.</h1></center>
+</body>
+</html>
