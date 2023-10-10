ef83af04a4bb5bf595a7ee2e0b6d9335e0198548
Samuel García <samueljose.garcia@iesdonana.org>
2023-10-10 15:45:44 +0200

    Reestructurado el código. Eliminado procesa_crea_libro.php y fusionado con crea_libro.php

--- biblioteca/crea_libro.php
+++ biblioteca/crea_libro.php
@@ -12,6 +12,120 @@
     <?php
     session_start();
     require_once 'aux.php';
+
+
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        $pdo = conectar('pgsql', 'localhost', 'biblioteca', 'biblioteca', 'biblioteca');
+        $codigo = obtener_parametro('codigo', $_POST);
+        $titulo = obtener_parametro('titulo', $_POST);
+        $autor = obtener_parametro('autor', $_POST);
+        $editorial = obtener_parametro('editorial', $_POST);
+        $anyo_publicacion = obtener_parametro('anyo_publicacion', $_POST);
+        $isbn = obtener_parametro('isbn', $_POST);
+        $id_categoria = obtener_parametro('categoria', $_POST);
+        $cantidad = obtener_parametro('cantidad', $_POST);
+
+
+
+        // Saneado.
+
+        $codigo = sanea($codigo);
+        $titulo = sanea($titulo);
+        $autor = sanea($autor);
+        $editorial = sanea($editorial);
+        $anyo_publicacion = sanea($anyo_publicacion);
+        $isbn = sanea($isbn);
+        $cantidad = sanea($cantidad);
+
+
+        // Validación.
+
+        if (!valida_enteros($codigo)) {
+            añade_error('El código introducido es incorrecto.');
+        }
+
+        if (!valida_texto($titulo)) {
+            añade_error('El título introducido no es válido.');
+        }
+
+        if (!valida_texto($autor)) {
+            añade_error('El autor introducido no es válido.');
+        }
+
+        if (!valida_texto($editorial)) {
+            añade_error('La editorial introducida no es válida.');
+        }
+
+        if (!valida_enteros($anyo_publicacion)) {
+            añade_error('El año de publiación introducido no es válido.');
+        }
+
+        if (!valida_isbn($isbn)) {
+            añade_error('El ISBN introducido no es válido.');
+        }
+
+        if (!valida_enteros($cantidad)) {
+            añade_error('La cantidad introducida no es válida.');
+        }
+
+        $errores = obtener_parametro('errores', $_SESSION);
+
+        // Comprobar si existen errores.
+        if (isset($errores)) {
+            if ($errores > 0) {
+                header('Location: biblioteca.php');
+            }
+        }
+
+        if (!$errores) {
+
+            $campos = [
+                ':codigo' => $codigo,
+                ':titulo' => $titulo,
+                ':autor' => $autor,
+                ':editorial' => $editorial,
+                ':anyo_publicacion' => $anyo_publicacion,
+                ':isbn' => $isbn,
+                ':cantidad' => $cantidad,
+            ];
+
+            // Para sacar el último id.
+            $sent = $pdo->prepare('INSERT INTO libros (codigo, titulo, autor, editorial, anyo_publicacion, isbn, cantidad)
+                            VALUES (:codigo, :titulo, :autor, :editorial, :anyo_publicacion, :isbn, :cantidad)
+                            RETURNING id');
+
+            $sent->execute($campos);
+
+            $ultimo_id = $sent->fetchColumn();
+
+            // Extraer el id_categoría
+
+
+            // Insertar el nuevo libro en la tabla libros_categorias (id_libro, id_categoria)
+            $sent = $pdo->prepare('INSERT INTO libros_categorias (id_libro, id_categoria)
+                                    VALUES (:ultimo_id, :id_categoria)');
+
+            var_dump($ultimo_id);
+            var_dump($id_categoria);
+
+
+            $sent->execute([
+                ':ultimo_id' => $ultimo_id,
+                ':id_categoria' => $id_categoria
+            ]);
+
+            if (!isset($_SESSION['exito_libro'])) {
+                $_SESSION['exito_libro'] = 'El libro se ha creado correctamente.';
+                header('Location: biblioteca.php');
+            }
+        }
+    }
+
+
+
+
     $pdo = conectar('pgsql', 'localhost', 'biblioteca', 'biblioteca', 'biblioteca');
     $sent = $pdo->query('SELECT DISTINCT * FROM categorias');
     $categorias = $sent->fetchAll();
@@ -21,13 +135,11 @@
         $categorias_unidimensional[$fila['id']] = $fila['nombre_categoria'];
     }
 
-    // var_dump($categorias_unidimensional);
-    // die();
 
     ?>
     <h1>Inserta un nuevo libro</h1>
     <div class="crea-formulario">
-        <form action="procesa_crea_libro.php" method="post">
+        <form action="" method="post">
             <input type="hidden" name="id_categoria" value="">
             <label for="codigo">Código</label>
             <input type="text" name="codigo" id="codigo" placeholder="Introduce el código">
@@ -43,7 +155,7 @@
             <input type="text" name="isbn" id="isbn" placeholder="Introduce el isbn">
             <label for="categoria">Categoría</label>
             <select name="categoria" id="categoria">
-                <?php foreach($categorias as $categoria): ?>
+                <?php foreach ($categorias as $categoria) : ?>
                     <option value="<?= $categoria[0] ?>"><?= $categoria[1] ?></option>
                 <?php endforeach; ?>
             </select>
