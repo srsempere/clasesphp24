163574335f3e25d347bb2b071f4b3d16ada4dc4d
samu <samueljose.garcia@iesdonana.org>
2023-10-19 17:46:28 +0200

    Validaciones del modificar departamento. Funcionando

--- empresa/departamentos/modificar.php
+++ empresa/departamentos/modificar.php
@@ -19,13 +19,13 @@
     }
 
     $id_departamento = isset($_GET['id']) ? $_GET['id'] : null;
+    $_SESSION['id_departamento'] = $id_departamento;
     $pdo = conectar();
-    $errores = isset($_SESSION['errores']) ? $_SESSION['errores'] : null;
+
 
     $sent = $pdo->prepare('SELECT COUNT(*) FROM departamentos WHERE id= :id_departamento');
     $sent->execute([':id_departamento' => $id_departamento]);
     $coincidencias = $sent->fetchColumn();
-    error_log("Coincidencias: $coincidencias");
 
     if ($coincidencias) { // El dpto. existe.
 
@@ -37,22 +37,36 @@
 
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-       $codigo = isset($_POST['codigo']) ? $_POST['codigo'] : null;
-       $denominacion = isset($_POST['denominacion']) ? $_POST['denominacion'] : null;
-       $localidad = isset($_POST['localidad']) ? $_POST['localidad'] : null;
-       $sent = $pdo->prepare('UPDATE departamentos
-                                SET codigo= :codigo, denominacion= :denominacion, localidad= :localidad
-                                WHERE id= :id');
-       $sent->execute([
-        ':codigo' => $codigo,
-        ':denominacion'=> $denominacion,
-        ':localidad' => $localidad,
-        ':id' => $id_departamento
-       ]);
+        $codigo = isset($_POST['codigo']) ? $_POST['codigo'] : null;
+        $denominacion = isset($_POST['denominacion']) ? $_POST['denominacion'] : null;
+        $localidad = isset($_POST['localidad']) ? $_POST['localidad'] : null;
+        $id_departamento = $_SESSION['id_departamento'];
 
-       add_success('El departamento se ha modificado correctamente');
-       header('Location: index.php');
-       exit();
+        if ($codigo && $denominacion) {
+            comprueba_codigo($codigo, $pdo, $id_departamento);
+            comprueba_denominacion($denominacion);
+            comprueba_localidad($localidad);
+
+            $errores = isset($_SESSION['errores']) ? $_SESSION['errores'] : false;
+
+            if ($errores) {
+                return ir_index();
+            }
+            $sent = $pdo->prepare('UPDATE departamentos
+                                              SET codigo= :codigo, denominacion= :denominacion, localidad= :localidad
+                                              WHERE id= :id');
+            $sent->execute([
+                ':codigo' => $codigo,
+                ':denominacion' => $denominacion,
+                ':localidad' => $localidad,
+                ':id' => $id_departamento
+            ]);
+
+            add_success('El departamento se ha modificado correctamente');
+            return ir_index();
+        } else {
+            return ir_index();
+        }
     }
     ?>
     <h1>Modificación de departamentos</h1>
@@ -69,7 +83,7 @@
         <a href="index.php">Volver a departamentos</a>
     <?php endforeach; ?>
     <?php
-    if (isset($errores)) :
+    if (isset($errores) && $errores) :
         unset($_SESSION['errores']);
         foreach ($errores as $error) :
     ?>
