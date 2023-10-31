8c92e814cfa85eff9e6c6d133ab575fd7a533868
Samu <samueljose.garcia@iesdonana.org>
2023-10-31 16:18:04 +0100

    Carga inicial. POO

---
+++ biblioteca_POO/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js,php}",
+            "./node_modules/flowbite/**/*.js"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
